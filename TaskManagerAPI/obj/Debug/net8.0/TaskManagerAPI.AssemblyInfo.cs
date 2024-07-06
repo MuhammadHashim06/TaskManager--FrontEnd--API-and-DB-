124aa93273b02dbaf18779777f3084016b02b3da
burhanHere <burhanburewala@gmail.com>
2024-07-06 15:48:09 +0500

    1356789

--- TaskManagerAPI/obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
+++ TaskManagerAPI/obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8338cf0e787ea7b547527ad208945ae7b2df175")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
